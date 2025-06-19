df69e0ce99c66ceacf486628d026f3a1740229f6
brimdor <chrisnelsonx@gmail.com>
2025-06-19 18:49:34 -0500

    fix: enhance chezmoi setup function with additional logging for repository initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,8 +84,10 @@ setup_chezmoi() {
         return 1
     fi
     if [ -n "${CHEZMOI_REPO}" ]; then
+        echo "Setting up Chezmoi with repository ${CHEZMOI_REPO} on branch ${CHEZMOI_BRANCH}."
         sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /home/coder/.local/bin
         chezmoi init --branch "$CHEZMOI_BRANCH" --apply "$CHEZMOI_REPO"
+        echo "Chezmoi initialized with repository ${CHEZMOI_REPO} on branch ${CHEZMOI_BRANCH}."
     fi
 }
 
